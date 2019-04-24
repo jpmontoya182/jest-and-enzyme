d7e9d996e8d8f0b5388175229a44fd8eae5716f5
Juan Pablo Montoya Cardona <jpmontoya182@gmail.com>
2019-04-24 17:14:07 -0500

    lecture 18

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const setup = (props = {}, state = null) => {
+	return shallow(<App {...props} />);
+};
+
+test('renders without error', () => {
+	const wrapper = shallow(<App />);
+	const appComponent = wrapper.find("[data-test='component-app']");
+	expect(appComponent.length).toBe(1);
 });
+
+test('renders increment button', () => {
+	const wrapper = shallow(<App />);
+	const button = wrapper.find("[data-test='increment-button']");
+	expect(button.length).toBe(1);
+});
+
+test('render counter display', () => {
+	const wrapper = shallow(<App />);
+	const counterDisplay = wrapper.find("[data-test='counter-display']");
+	expect(counterDisplay.length).toBe(1);
+});
+
+test('counter starts at 0', () => {});
+
+test('clicking button increments counter display ', () => {});
